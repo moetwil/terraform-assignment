ae912cdbe63dd9c69a97f4da9effe731940a094a
Luc Moetwil <89918194+moetwil@users.noreply.github.com>
2024-05-28 16:30:30 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -50,7 +50,7 @@ resource "azurerm_network_interface" "main" {
   resource_group_name = azurerm_resource_group.main.name
 
   ip_configuration {
-    name                          = "ipconfig-terraform-assignment-group-131"
+    name                          = "ipconfig-terraform-assignment-group-13"
     subnet_id                     = azurerm_subnet.main.id
     private_ip_address_allocation = "Dynamic"
     public_ip_address_id          = azurerm_public_ip.main.id
